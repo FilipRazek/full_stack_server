76d46add87788d4b4f04c8b633adeb6c8b3240db
Filip Razek <filip.razek@polyconseil.fr>
2023-09-18 17:29:40 +0200

    Modify app.js from the outside world

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer(function (req, res) {
-	res.write("On my way to full-stack!");
+	res.write("Edited by the outside world!");
 	res.end();
 }).listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
