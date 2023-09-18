962e71e7f5c95606d14fab79c7511810aae382d5
Filip Razek <filip.razek@polyconseil.fr>
2023-09-18 17:37:56 +0200

    Remove useless variable

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const PORT = 3000;
 
-const server = http.createServer(function (req, res) {
+http.createServer(function (req, res) {
 	res.write("Edited by the outside world!");
 	res.end();
 }).listen(PORT, () => console.log(`Listening on port ${PORT}`));
