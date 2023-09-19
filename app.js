fcc5a7c47f8448c718779e622796098668eadd28
Filip Razek <filip.razek@polyconseil.fr>
2023-09-19 10:36:06 +0200

    Send index.html instead of plain text

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const http = require('http');
+const fs = require('fs');
 
 const PORT = 3000;
 
-http.createServer(function (req, res) {
-	res.write("Edited by the outside world!");
-	res.end();
-}).listen(PORT, () => console.log(`Listening on port ${PORT}`));
+const server = http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	fs.createReadStream("index.html").pipe(res);
+})
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 
